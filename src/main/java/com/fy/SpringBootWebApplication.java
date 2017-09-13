c8b1f5ab3396a076897b53c58d5f01d45565b95d
SaiLoy-Lee <512490225@qq.com>
2017-09-13 09:12:12 +0800

    项目初始化

---
+++ src/main/java/com/fy/SpringBootWebApplication.java
@@ -0,0 +1,14 @@
+package com.fy;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by Administrator on 2017/9/13.
+ */
+public class SpringBootWebApplication {
+
+    public static void main (String[] args){
+        SpringApplication.run(SpringBootWebApplication.class,args);
+    }
+
+}
