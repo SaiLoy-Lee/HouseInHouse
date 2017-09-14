d284d7ac47067b36f83a249bcab1a67c1731baa7
RitaGH <1309492143@qq.com>
2017-09-14 20:13:41 +0800

    更新User控制层，业务层和持久层，可信度不高，若发现很多bug，欢迎更改

--- src/main/java/com/fy/controller/UserController.java
+++ src/main/java/com/fy/controller/UserController.java
@@ -1,12 +1,64 @@
 package com.fy.controller;
 
+import com.fy.pojo.Dept;
+import com.fy.pojo.User;
+import com.fy.service.DeptService;
+import com.fy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/9/13.
  */
 @Controller
+@RequestMapping("/pages/sysadmin/user")
 public class UserController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private DeptService deptService;
+    @RequestMapping("/list")
+    @ResponseBody
+    public String findAll(Model model){
+//        List<User> userList = userService.findAll();
+//        model.addAttribute("userList",userList);
+        return "/pages/sysadmin/user/jUserList";
+    }
+   @RequestMapping("/start")
+    public String toStart(@RequestParam(value = "hhUserId",required = true)String[] hhUserIds ){
+        int hhUserStatus = 1;
+        userService.updateStatus(hhUserIds,hhUserStatus);
+        return "redirect:/pages/sysadmin/user/list";
+    }
+    @RequestMapping("/stop")
+    public String toStop(@RequestParam(value = "hhUserId",required = true)String[] hhUserIds ){
+        int hhUserStatus = 0;
+        userService.updateStatus(hhUserIds,hhUserStatus);
+        return "redirect:/pages/sysadmin/user/list";
+    }
+    @RequestMapping("/delete")
+    public String toDelete(@RequestParam(value = "hhUserId",required=true)String hhUserIds){
+        userService.deleteUser(hhUserIds);
+        return "redirect:/pages/sysadmin/user/list";
+    }
+    @RequestMapping("/tocreate")
+    public String toCreate(Model model){
+        List<Dept> deptList = deptService.findAll();
+        model.addAttribute("deptList",deptList);
 
+
+        return "/pages/sysadmin/user/jUserCreate";
+    }
+    @RequestMapping("/save")
+    public String saveUser(User user){
+        userService.saveUser(user);
+        return "redirect:/pages/sysadmin/user/list";
+    }
 
 }
--- src/main/java/com/fy/service/UserService.java
+++ src/main/java/com/fy/service/UserService.java
@@ -1,7 +1,18 @@
 package com.fy.service;
 
+import com.fy.pojo.User;
+
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/9/13.
  */
 public interface UserService {
+    public List<User> findAll();
+
+    public void updateStatus(String[] hhUserIds, int hhUserStatus);
+
+    public void deleteUser(String hhUserIds);
+
+    public void saveUser(User user);
 }
