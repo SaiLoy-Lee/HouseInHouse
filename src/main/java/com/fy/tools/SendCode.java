53e3d391669c178bacfc0a687d673ca002dc0fb2
czhsun <chenzhenhuai@163.com>
2017-09-15 12:48:04 +0800

    提交chart图表页面和chart控制文件,以及echarts.js库

--- src/main/java/com/fy/tools/SendCode.java
+++ src/main/java/com/fy/tools/SendCode.java
@@ -1,151 +1,151 @@
-//package com.fy.tools;
-//
-///**
-// * Created by ryp on 2017/9/14.
-// *
-// * 短信验证码验证
-// */
-//import com.aliyuncs.DefaultAcsClient;
-//import com.aliyuncs.IAcsClient;
-//import com.aliyuncs.dysmsapi.model.v20170525.QuerySendDetailsRequest;
-//import com.aliyuncs.dysmsapi.model.v20170525.QuerySendDetailsResponse;
-//import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
-//import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
-//import com.aliyuncs.dysmsapi.transform.v20170525.SendSmsResponseUnmarshaller;
-//import com.aliyuncs.exceptions.ClientException;
-//import com.aliyuncs.http.FormatType;
-//import com.aliyuncs.http.HttpResponse;
-//import com.aliyuncs.profile.DefaultProfile;
-//import com.aliyuncs.profile.IClientProfile;
-//import com.fy.pojo.SMessage;
-//
-//import java.nio.charset.Charset;
-//import java.text.SimpleDateFormat;
-//import java.util.Date;
-//import java.util.UUID;
-//
-//public class SendCode {
-//
-//
-//    public static int sendSms(SMessage sMessage) throws ClientException, InterruptedException{
-//        String type = sMessage.getsMessageType();
-//        if(type.equals("1")||type=="1"){
-//            int mobile_code = (int) ((Math.random()*9+1)*100000);
-//            SendSmsResponse response = sendSmsVerificationCode(sMessage,mobile_code);
-//            return mobile_code;
-//        }else if(type.equals("2")||type=="2"){
-//            SendSmsResponse response = sendSmsCancel(sMessage);
-//            return 0;//0表示成功
-//        }else if (type.equals("3")||type=="3"){
-//            SendSmsResponse response= sendSmsPayment(sMessage);
-//
-//            return  0;
-//
-//        }else{
-//            return 1;//表示失败
-//        }
-//
-//
-//    }
-//
-//
-//    //产品名称:云通信短信API产品,开发者无需替换
-//    static final String product = "Dysmsapi";
-//    //产品域名,开发者无需替换
-//    static final String domain = "dysmsapi.aliyuncs.com";
-//
-//    // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
-//    static final String accessKeyId = "LTAIuiBPEAGp3ePe";
-//    static final String accessKeySecret = "VPRaNlnNC9svlQrAjBkdf8JxLqfnfC";
-//
-//    public static SendSmsResponse sendSmsVerificationCode (SMessage sMessage,int code) throws ClientException {
-//
-//
-//        String phoneNumber=sMessage.getsMessageCell();
-//
-//        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");//不必修改
-//        System.setProperty("sun.net.client.defaultReadTimeout", "10000");//不必修改
-//
-//        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);//"***"分别填写自己的AccessKey ID和Secret
-//
-//        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);//不必修改
-//
-//        IAcsClient acsClient = new DefaultAcsClient(profile);//不必修改
-//
-//        SendSmsRequest request = new SendSmsRequest();//不必修改
-//
-//
-//        request.setPhoneNumbers(phoneNumber);//****处填写接收方的手机号码
-//
-//        request.setSignName("房中房");//此处填写已申请的短信签名
-//
-//        request.setTemplateCode("SMS_95760031");//此处填写获得的短信模版CODE
-//
-//        request.setTemplateParam("{\"code\":\""+code+"\"}");//笔者的短信模版中有${code}, 因此此处对应填写验证码
-//
-//        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);//不必修改
-//
-//        return sendSmsResponse;
-//    }
-//
-//
-//    public static SendSmsResponse sendSmsCancel(SMessage sMessage) throws ClientException {
-//
-//        String phoneNumber=sMessage.getsMessageCell();
-//        String name=sMessage.getsMessageRecipients();
-//        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");//不必修改
-//        System.setProperty("sun.net.client.defaultReadTimeout", "10000");//不必修改
-//
-//        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);//"***"分别填写自己的AccessKey ID和Secret
-//
-//        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);//不必修改
-//
-//        IAcsClient acsClient = new DefaultAcsClient(profile);//不必修改
-//
-//        SendSmsRequest request = new SendSmsRequest();//不必修改
-//
-//        request.setPhoneNumbers(phoneNumber);//****处填写接收方的手机号码
-//
-//        request.setSignName("房中房");//此处填写已申请的短信签名
-//
-//        request.setTemplateCode("SMS_95710024");//此处填写获得的短信模版CODE
-//
-//        request.setTemplateParam("{\"name\":\""+name+"\"}");//笔者的短信模版中有${code}, 因此此处对应填写验证码
-//
-//        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);//不必修改
-//
-//        return sendSmsResponse;
-//    }
-//
-//    public static SendSmsResponse sendSmsPayment(SMessage sMessage) throws ClientException {
-//
-//        String phoneNumber=sMessage.getsMessageCell();
-//        String name=sMessage.getsMessageRecipients();
-//        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");//不必修改
-//        System.setProperty("sun.net.client.defaultReadTimeout", "10000");//不必修改
-//
-//        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);//"***"分别填写自己的AccessKey ID和Secret
-//
-//        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);//不必修改
-//
-//        IAcsClient acsClient = new DefaultAcsClient(profile);//不必修改
-//
-//        SendSmsRequest request = new SendSmsRequest();//不必修改
-//
-//        request.setPhoneNumbers(phoneNumber);//****处填写接收方的手机号码
-//
-//        request.setSignName("房中房");//此处填写已申请的短信签名
-//
-//        request.setTemplateCode("SMS_96165019");//此处填写获得的短信模版CODE
-//
-//        request.setTemplateParam("{\"name\":\""+name+"\"}");//笔者的短信模版中有${code}, 因此此处对应填写验证码
-//
-//        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);//不必修改
-//
-//        return sendSmsResponse;
-//    }
-//
-//
-//
-//
-//}
+package com.fy.tools;
+
+/**
+ * Created by ryp on 2017/9/14.
+ *
+ * 短信验证码验证
+ */
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.QuerySendDetailsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.QuerySendDetailsResponse;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.dysmsapi.transform.v20170525.SendSmsResponseUnmarshaller;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.http.FormatType;
+import com.aliyuncs.http.HttpResponse;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
+import com.fy.pojo.SMessage;
+
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+public class SendCode {
+
+
+    public static int sendSms(SMessage sMessage) throws ClientException, InterruptedException{
+        String type = sMessage.getsMessageType();
+        if(type.equals("1")||type=="1"){
+            int mobile_code = (int) ((Math.random()*9+1)*100000);
+            SendSmsResponse response = sendSmsVerificationCode(sMessage,mobile_code);
+            return mobile_code;
+        }else if(type.equals("2")||type=="2"){
+            SendSmsResponse response = sendSmsCancel(sMessage);
+            return 0;//0表示成功
+        }else if (type.equals("3")||type=="3"){
+            SendSmsResponse response= sendSmsPayment(sMessage);
+
+            return  0;
+
+        }else{
+            return 1;//表示失败
+        }
+
+
+    }
+
+
+    //产品名称:云通信短信API产品,开发者无需替换
+    static final String product = "Dysmsapi";
+    //产品域名,开发者无需替换
+    static final String domain = "dysmsapi.aliyuncs.com";
+
+    // TODO 此处需要替换成开发者自己的AK(在阿里云访问控制台寻找)
+    static final String accessKeyId = "LTAIuiBPEAGp3ePe";
+    static final String accessKeySecret = "VPRaNlnNC9svlQrAjBkdf8JxLqfnfC";
+
+    public static SendSmsResponse sendSmsVerificationCode (SMessage sMessage,int code) throws ClientException {
+
+
+        String phoneNumber=sMessage.getsMessageCell();
+
+        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");//不必修改
+        System.setProperty("sun.net.client.defaultReadTimeout", "10000");//不必修改
+
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);//"***"分别填写自己的AccessKey ID和Secret
+
+        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);//不必修改
+
+        IAcsClient acsClient = new DefaultAcsClient(profile);//不必修改
+
+        SendSmsRequest request = new SendSmsRequest();//不必修改
+
+
+        request.setPhoneNumbers(phoneNumber);//****处填写接收方的手机号码
+
+        request.setSignName("房中房");//此处填写已申请的短信签名
+
+        request.setTemplateCode("SMS_95760031");//此处填写获得的短信模版CODE
+
+        request.setTemplateParam("{\"code\":\""+code+"\"}");//笔者的短信模版中有${code}, 因此此处对应填写验证码
+
+        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);//不必修改
+
+        return sendSmsResponse;
+    }
+
+
+    public static SendSmsResponse sendSmsCancel(SMessage sMessage) throws ClientException {
+
+        String phoneNumber=sMessage.getsMessageCell();
+        String name=sMessage.getsMessageRecipients();
+        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");//不必修改
+        System.setProperty("sun.net.client.defaultReadTimeout", "10000");//不必修改
+
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);//"***"分别填写自己的AccessKey ID和Secret
+
+        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);//不必修改
+
+        IAcsClient acsClient = new DefaultAcsClient(profile);//不必修改
+
+        SendSmsRequest request = new SendSmsRequest();//不必修改
+
+        request.setPhoneNumbers(phoneNumber);//****处填写接收方的手机号码
+
+        request.setSignName("房中房");//此处填写已申请的短信签名
+
+        request.setTemplateCode("SMS_95710024");//此处填写获得的短信模版CODE
+
+        request.setTemplateParam("{\"name\":\""+name+"\"}");//笔者的短信模版中有${code}, 因此此处对应填写验证码
+
+        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);//不必修改
+
+        return sendSmsResponse;
+    }
+
+    public static SendSmsResponse sendSmsPayment(SMessage sMessage) throws ClientException {
+
+        String phoneNumber=sMessage.getsMessageCell();
+        String name=sMessage.getsMessageRecipients();
+        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");//不必修改
+        System.setProperty("sun.net.client.defaultReadTimeout", "10000");//不必修改
+
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);//"***"分别填写自己的AccessKey ID和Secret
+
+        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);//不必修改
+
+        IAcsClient acsClient = new DefaultAcsClient(profile);//不必修改
+
+        SendSmsRequest request = new SendSmsRequest();//不必修改
+
+        request.setPhoneNumbers(phoneNumber);//****处填写接收方的手机号码
+
+        request.setSignName("房中房");//此处填写已申请的短信签名
+
+        request.setTemplateCode("SMS_96165019");//此处填写获得的短信模版CODE
+
+        request.setTemplateParam("{\"name\":\""+name+"\"}");//笔者的短信模版中有${code}, 因此此处对应填写验证码
+
+        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);//不必修改
+
+        return sendSmsResponse;
+    }
+
+
+
+
+}
---
+++ src/main/webapp/WEB-INF/pages/chart/left.jsp
@@ -0,0 +1,43 @@
+<%@ page language="java" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://shiro.apache.org/tags" prefix="shiro" %>
+<%@ include file="../baselist.jsp" %>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+    <link rel="stylesheet" rev="stylesheet" type="text/css" href="${ctx}/staticfile/skin/default/css/left.css" media="all"/>
+</head>
+ 
+<body id="left_frame">
+<div class="PositionFrame_black" id="PositionFrame"></div>
+ 
+ 
+<!-- begin1  -->
+<div id="sidebar" class="sidebar">
+	<div class="sidebar_t">
+		<div class="sidebar_t_l"></div>
+		<div class="sidebar_t_c"></div>
+		<div class="sidebar_t_r"></div>
+	</div>
+        <div class="panel">
+        <div class="panel_icon"><img src="${ctx}/staticfile/skin/default/images/icon/user1_lock.png"/></div>
+        <div class="panel-header">
+        <div class="panel-title">图表统计</div>
+        <div class="panel-content">
+			<ul>
+					<li><a href="${ctx}/chart/chartMultiX" onclick="linkHighlighted(this)" target="main" id="aa_1">房源概况</a></li>
+					<li><a href="${ctx}/chart/chartRadar" onclick="linkHighlighted(this)" target="main" id="aa_1">租金占比图</a></li>
+					<li><a href="${ctx}/chart/chartBars" onclick="linkHighlighted(this)" target="main" id="aa_1">房屋来源占比图</a></li>
+			</ul>
+        </div>
+        </div>
+    </div>
+    <div class="sidebar_t">
+		<div class="sidebar_b_l"></div>
+		<div class="sidebar_t_c"></div>
+		<div class="sidebar_b_r"></div>
+	</div>  
+</div>	
+
+
+</body>
+</html>
