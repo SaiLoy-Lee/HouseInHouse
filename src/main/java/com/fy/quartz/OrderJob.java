bdfc4750f027887b98c6d41ee90e2263318cdebc
jxzx2 <794375334@qq.com>
2017-09-15 16:24:28 +0800

    分支提交增加 定时任务类UserJob、OrderJob, 静态文件

---
+++ src/main/java/com/fy/quartz/OrderJob.java
@@ -0,0 +1,15 @@
+package com.fy.quartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * Created by Administrator on 2017/9/14.
+ */
+public class OrderJob implements Job{
+    @Override
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+
+    }
+}
